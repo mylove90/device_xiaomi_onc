5c5d705707b2f9feb5780b6184f8acfcf435b7a9
mylove90 <engm.saad1990@gmail.com>
2019-10-17 02:55:02 +0200

    Onc: XenonHD

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onc.mk
+    $(LOCAL_DIR)/xenonhd_onc.mk
--- xenonhd_onc.mk
+++ xenonhd_onc.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/onc/device.mk)
 
  # Inherit some common Lineage stuff.	
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
  # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_onc
+PRODUCT_NAME := xenonhd_onc
 PRODUCT_DEVICE := onc
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
