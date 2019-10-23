e9ebcc70d94a45548fb661ef71f6478b566a5c46
RevanthTolety <crevanthtolety@gmail.com>
2019-10-23 14:09:01 +0530

    onc: Import Wifi Config

--- device.mk
+++ device.mk
@@ -109,6 +109,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+# Prebuilt
+    $(LOCAL_PATH)/prebuilt/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+
 # Telephony
 PRODUCT_BOOT_JARS += \
     telephony-ext
